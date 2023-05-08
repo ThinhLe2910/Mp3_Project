f9a2b012b95f3cbaf6ea1a3e0430050693878552
Thinh Le <thinh.le089@gmail.com>
2023-05-08 17:42:59 +0700

    check token

--- mp3/Tabbar/TabBarViewController.swift
+++ mp3/Tabbar/TabBarViewController.swift
@@ -10,19 +10,33 @@ import UIKit
 class TabBarViewController: UITabBarController {
     
     var musicApiService : MusicApiService!
-    var categoryApiService : CategoryApiService!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        musicApiService = MusicApi()
-        categoryApiService = CategoryApi()
-        let viewOnline = HomeViewController(musicApi: musicApiService,categoryAPI: categoryApiService)
-        viewOnline.tabBarItem = UITabBarItem(title: "Online", image: UIImage(named: "music"), tag: 0)
-        let navviewOnline = UINavigationController(rootViewController: viewOnline)
-        let viewAccount = CheckAccountViewController()
-        viewAccount.tabBarItem = UITabBarItem(title: "Account", image: UIImage(named: "account-tab"), tag: 1)
-        let navviewAccount = UINavigationController(rootViewController: viewAccount)
-            self.viewControllers = [navviewOnline, navviewAccount]
-    }
+      var categoryApiService : CategoryApiService!
+      var accountApiService : AccountAPIService!
+      var navviewDashboard:UINavigationController!
+      override func viewDidLoad() {
+          super.viewDidLoad()
+          accountApiService = AccountApi()
+          musicApiService = MusicApi()
+          categoryApiService = CategoryApi()
+          let viewOnline = HomeViewController(musicApi: musicApiService,categoryAPI: categoryApiService)
+          viewOnline.tabBarItem = UITabBarItem(title: "Online", image: UIImage(named: "music"), tag: 0)
+          let navviewOnline = UINavigationController(rootViewController: viewOnline)
+          
+          let login = LoginViewController(accountAPI: accountApiService)
+          login.tabBarItem = UITabBarItem(title: "Account", image: UIImage(named: "account-tab"), tag: 1)
+          
+          let dashboard = DashboardViewController(accountAPI: accountApiService)
+          dashboard.tabBarItem = UITabBarItem(title: "Account", image: UIImage(named: "account-tab"), tag: 1)
+          
+          if let _ = UserDefaults.standard.string(forKey: "token") {
+              navviewDashboard = UINavigationController(rootViewController: dashboard)
+          } else {
+              navviewDashboard = UINavigationController(rootViewController: login)
+          }
+          self.viewControllers = [navviewOnline, navviewDashboard]
+          
+          
+      }
     
     
 }
